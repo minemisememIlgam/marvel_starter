28fc0cb055927c778c3a7ead4209d8dc27c1fb82
minemisememIlgam <mindubaev1998@gmail.com>
2023-04-18 15:38:57 +0000

    state lifting up

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -7,14 +8,19 @@ import decoration from "../../resources/img/vision.png";
 
 const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
 const App = () => {
+  const [selectedChar, setSelectedChar] = useState(null);
+  const handleCharClick = (char) => {
+    setSelectedChar(char);
+  };
+
   return (
     <div className="app">
       <AppHeader />
       <main>
         <RandomChar id={id} />
         <div className="char__content">
-          <CharList id={id} />
-          <CharInfo />
+          <CharList id={id} onCharClick = {handleCharClick} />
+          <CharInfo selectChar = {selectedChar} />
         </div>
         <img className="bg-decoration" src={decoration} alt="vision" />
       </main>
